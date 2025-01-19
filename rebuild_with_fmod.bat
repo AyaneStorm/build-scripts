2499e68437cd789eb007196e4e7d891c5d4fa139
chanayane <chanayane83@gmail.com>
2025-01-19 16:24:16 +0100

    fix

--- rebuild_with_fmod.bat
+++ rebuild_with_fmod.bat
@@ -2,4 +2,5 @@ call set_ayanestorm_vars.bat
 set PATH=C:\CMake\bin;C:\cygwin64\bin;C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\;%PATH%
 call set_firestorm_vars.bat
 call set_fmod_vars.bat
+cd %AYANESTORM_DIR%\..
 call .venv\Scripts\activate.bat
